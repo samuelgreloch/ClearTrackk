04e5767aae9a2d642dc33ae2942a9ec6d4f06cbf
sgreloch <samuelgreloch@yahoo.com>
2024-12-18 16:32:34 -0500

    finished CRUD API

--- src/main/java/ClearTrack/backend/ReceiptController.java
+++ src/main/java/ClearTrack/backend/ReceiptController.java
@@ -27,4 +27,26 @@ public class ReceiptController {
         receiptRepository.save(receipt);
         return "Receipt saved";
     }
+
+    @PutMapping(value = "update/{id}")
+    public String updateReceipt(@PathVariable long id, @RequestBody Receipt receipt) {
+        Receipt updatedReceipt = receiptRepository.findById(id).get();
+        updatedReceipt.setPrice(receipt.getPrice());
+        updatedReceipt.setQuantity(receipt.getQuantity());
+        updatedReceipt.setDate(receipt.getDate());
+        updatedReceipt.setItemName(receipt.getItemName());
+        updatedReceipt.setVendorName(receipt.getVendorName());
+        updatedReceipt.setTotalPrice(receipt.getTotalPrice());
+        receiptRepository.save(updatedReceipt);
+        return "Receipt updated";
+        }
+
+        @DeleteMapping(value="delete/{id}")
+        public String deleteReceipt(@PathVariable long id) {
+         Receipt deleteReceipt = receiptRepository.findById(id).get();
+         receiptRepository.delete(deleteReceipt);
+         return "Receipt deleted"+ id;
+        }
+
+
 }
