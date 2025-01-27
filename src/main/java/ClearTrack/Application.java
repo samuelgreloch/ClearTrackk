d1c3dace6713662327cc683554b5e7ffd41edbe5
samuelgreloch <samuelgreloch@yahoo.com>
2025-01-27 18:37:47 -0500

    adding cors

--- src/main/java/ClearTrack/Application.java
+++ src/main/java/ClearTrack/Application.java
@@ -1,13 +1,30 @@
 package ClearTrack;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+
 }
 
+@Bean
+public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true); // you can change this as per your requirements
+    config.setAllowedOrigins(Arrays.asList("*")); // this allows all origins
+    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+    source.registerCorsConfiguration("/**", config); // /** applies the configuration to all paths
+    return new CorsFilter(source);
+}
+}
